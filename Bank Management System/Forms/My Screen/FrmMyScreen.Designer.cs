9fa05f8152a6d489ec0d3b07b7380276a75ae679
Pham Tung Duong <phamtungduong763@gmail.com>
2025-06-20 14:50:03 +0700

    Add Screen

---
+++ Bank Management System/Forms/My Screen/FrmMyScreen.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Bank_Management_System.Forms.My_Screen
+{
+    partial class FrmMyScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmMyScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 555);
+            this.Name = "FrmMyScreen";
+            this.Text = "FrmMyScreen";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}

c5cb901026de3b59904bc1912ceba2cd106c197f
Pham Tung Duong <phamtungduong763@gmail.com>
2025-06-20 14:40:31 +0700

    feat(dal): add MySQL connection configuration

---
+++ BankDataAccessLayer/clsDataAccessSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankDataAccessLayer
+{
+    public class clsDataAccessSettings
+    {
+        public static string ConnectionString = "Server=localhost;Port=3306;Database=BankDB;Uid=root;Pwd=senti123;Charset=utf8mb4;SslMode=Required;";
+    }
+}

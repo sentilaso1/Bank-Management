1c0fe69d96b89f5c6b0fc1f51d5c6051d697e3c3
sentilaso1 <166209447+sentilaso1@users.noreply.github.com>
2025-06-21 11:33:58 +0700

    Complete clients module views

---
+++ BankManagementWPF/Views/HomeView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace BankManagementSystem.WPF.Views
+{
+    public partial class HomeView : UserControl
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+        }
+    }
+}

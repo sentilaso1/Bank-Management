ac87848710a7f835da4c8bfd620b3096b605ea8b
sentilaso1 <166209447+sentilaso1@users.noreply.github.com>
2025-06-21 11:50:34 +0700

    Add transactions hub and deposit functionality

---
+++ BankManagementWPF/Views/ClientsListView.xaml.cs
@@ -0,0 +1,51 @@
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using BankBusinessLayer;
+
+namespace BankManagementSystem.WPF.Views
+{
+    public partial class ClientsListView : UserControl
+    {
+        private DataTable _clientsTable;
+
+        public ClientsListView()
+        {
+            InitializeComponent();
+            LoadClients();
+        }
+
+        private void LoadClients()
+        {
+            _clientsTable = Client.GetAllClients();
+            ClientsDataGrid.ItemsSource = _clientsTable.DefaultView;
+        }
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            LoadClients();
+        }
+
+        private void ApplyFilter_Click(object sender, RoutedEventArgs e)
+        {
+            if (_clientsTable == null)
+                return;
+
+            var view = _clientsTable.AsEnumerable();
+            switch (FilterComboBox.SelectedIndex)
+            {
+                case 1: // Active Accounts - balance > 0
+                    view = view.Where(r => r.Field<decimal>("Balance") > 0);
+                    break;
+                case 2: // High Balance
+                    view = view.Where(r => r.Field<decimal>("Balance") > 10000);
+                    break;
+                default:
+                    break;
+            }
+            DataTable filtered = view.Any() ? view.CopyToDataTable() : _clientsTable.Clone();
+            ClientsDataGrid.ItemsSource = filtered.DefaultView;
+        }
+    }
+}
